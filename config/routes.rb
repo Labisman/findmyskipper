72e166bbd3841a918d635394b975e1b31a35d978
Peter Lay <peterlaywaugh@gmail.com>
2023-08-21 16:02:14 +0200

    Changed homepage to listings index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "listings#index"
   resources :listings do
     resources :bookings, only: [:new, :create, :update]
   end
