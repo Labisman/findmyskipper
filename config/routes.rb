6552673eff19aab6cd698b104467092595aa251a
Peter Lay <peterlaywaugh@gmail.com>
2023-08-21 16:13:22 +0200

    Added additional actions to listings routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "listings#index"
   resources :listings do
-    resources :bookings, only: [:new, :create, :update]
+    resources :bookings, only: [:new, :create, :update, :edit, :index]
   end
   resources :bookings, only: [:destroy]
 end
