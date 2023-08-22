4b104e26f339487eb5daac566ee760dfa4d188e0
Peter Lay <peterlaywaugh@gmail.com>
2023-08-22 16:44:02 +0200

    Changed authentication to allow users to view listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
 def index
   @listings = Listing.all
