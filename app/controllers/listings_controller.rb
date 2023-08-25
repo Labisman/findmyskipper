b02f1fd78dc720799092f83c1b5afd86a4bc8579
Peter Lay <peterlaywaugh@gmail.com>
2023-08-25 11:44:40 +0200

    Fixed indentation in listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,30 +2,31 @@ class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-def index
-  @listings = Listing.all
-  if params[:query].present?
-    sql_subquery = "title ILIKE :query OR listing_address ILIKE :query"
-    @listings = @listings.where(sql_subquery, query: "%#{params[:query]}%")
+  def index
+    @listings = Listing.all
+      if params[:query].present?
+        sql_subquery = "title ILIKE :query OR listing_address ILIKE :query"
+        @listings = @listings.where(sql_subquery, query: "%#{params[:query]}%")
+      end
   end
-end
 
-def show
-end
+  def show
+  end
 
-def new
-  @listing = Listing.new
-end
+  def new
+    @listing = Listing.new
+  end
 
-def create
-  @listing = Listing.new(listing_params)
-  @listing.user = current_user
-  respond_to do |format|
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user = current_user
+    respond_to do |format|
     if params[:listing][:photos].present?
       params[:listing][:photos].each do |photo|
         @listing.photos.attach(photo)
       end
     end
+
     if @listing.save
       format.html { redirect_to listing_url(@listing), notice: "Listing was successfully created." }
       format.json { render :show, status: :created, location: @listing }
@@ -33,19 +34,19 @@ def create
       format.html { render :new, status: :unprocessable_entity }
       format.json { render json: @listing.errors, status: :unprocessable_entity }
     end
+    end
   end
-end
 
-def edit
-end
+  def edit
+  end
 
   def update
     if params[:listing][:photos].present?
       params[:listing][:photos].each do |photo|
-      @listing.photos.attach(photo)
+        @listing.photos.attach(photo)
+      end
     end
-    end
-    if @listing.update(listing_params)
+    if @listing.update(listin g_params)
       redirect_to @listing, notice: "Listing was successfully updated."
     else
       render :edit
